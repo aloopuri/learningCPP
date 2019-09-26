dbc127d116e60a618d5fc4904aae2bdff7677e01
Hassan <k1889317@nmscde004064.nms.kcl.ac.uk>
2019-09-26 19:04:18 +0100

    Prelim Done :)

--- Part1/Item.h
+++ Part1/Item.h
@@ -18,6 +18,7 @@ private:
 	double longitude;
 	string ID;
 	int time;
+    int R = 6373000;
 
 public:
 	Item(double latitudeIn, double longitudeIn, string IDIn, int timeIn):
@@ -29,8 +30,18 @@ public:
         o << "{" << latitude << ", " << longitude << ", \"" << ID << "\", " << time << "}";
     }
 
-    double distanceTo(Item itemIn) {
-        
+    double distanceTo(Item & itemIn) {
+        double dlon = convertRad(itemIn.longitude- longitude);
+        double dlat = convertRad(itemIn.latitude - latitude);
+
+        double a = pow((sin(dlat/2)), 2) + cos(convertRad(itemIn.latitude)) * cos(convertRad(latitude)) * pow((sin(dlon/2)), 2);
+        double c = 2 * atan2( sqrt(a), sqrt(1-a) );
+        double distance = R * c;
+        return distance;
+    }
+
+    double convertRad(double deg) {
+        return (deg * M_PI)/180;
     }
 
 };
