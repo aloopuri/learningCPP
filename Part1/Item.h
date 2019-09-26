42d005fdfbc5daa96650d3e1ea2dbfca5605d915
Hassan <k1889317@nmscde004064.nms.kcl.ac.uk>
2019-09-26 17:42:56 +0100

    init commit

--- Part1/Item.h
+++ Part1/Item.h
@@ -4,10 +4,25 @@
 #include <iostream>
 using std::ostream;
 
+#include <string>
+using std::string;
+
 // TODO: your code  goes here
 
+class Item {
 
+private:
+	double latitude;
+	double longitude;
+	string ID;
+	int time;
 
+public:
+	Item(double latitudeIn, double longitudeIn, string IDIn, int timeIn):
+	latitude(latitudeIn), longitude(longitudeIn), ID(IDIn), time(timeIn) {
+	
+	}
+};
 
 
 
