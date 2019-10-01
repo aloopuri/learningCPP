a28d2259f3c5fb1bb570bf6ee780e4abd5a5d129
Hassan <k1889317@nmscde004015.nms.kcl.ac.uk>
2019-10-01 19:24:24 +0100

    stack in prog

--- Part2/Stack.h
+++ Part2/Stack.h
@@ -4,9 +4,43 @@
 #include <vector>
 using std::vector;
 
+#include <string>
+using std::string;
+
 // TODO: Your code goes here
 
+class Stack {
 
+private:
+    vector<double> stack;
+
+public:
+    Stack(){
+        
+    }
+
+    const bool empty(){
+        if(stack.size() == 0){
+            return true;
+        }
+        return false;
+    }
+
+    double pop(){
+
+    }
+
+    void push(double num){
+        stack.push_back(num);
+    }
+
+    
+
+};
+
+double evaluate(string operation){
+
+    }
 
 // Do not write anything below this line
 
