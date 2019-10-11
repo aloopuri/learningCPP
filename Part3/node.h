c90e87c05f3e20cd1e2555671d031bb05c6b4f48
Hassan <k1889317@nmscde004027.nms.kcl.ac.uk>
2019-10-11 19:06:42 +0100

    node + operators done

--- Part3/node.h
+++ Part3/node.h
@@ -11,8 +11,22 @@ using std::endl;
 // TODO your code for the Node class goes here:
 // (Note the 'NodeIterator' class will report lots of errors until Node exists
 
+template<typename T>
+class Node {
 
+public:
 
+    T data;
+    Node<T>* next;
+    Node<T>* previous;
+
+    Node(T dataIn){
+        data = dataIn;
+        next = nullptr;
+        previous = nullptr;
+
+    }
+};
 
 
 template<typename T>
@@ -35,7 +49,18 @@ public:
 
     // TODO: complete the code for NodeIterator here
         
-    
+    void operator++(){
+        current = current->next;
+    }
+
+    bool operator==(const NodeIterator<T> & other) const{
+        return current == other.current;
+    }
+
+    bool operator!=(const NodeIterator<T> & other) const{
+        return current != other.current;
+    }
+
 };
 
 // do not edit below this line
