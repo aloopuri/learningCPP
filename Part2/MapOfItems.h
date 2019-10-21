eacf3a8c3a9b3bc258f4b7f3a2624dc03b1bf8b0
Hassan <k1889317@nmscde004164.nms.kcl.ac.uk>
2019-10-21 20:34:07 +0100

    i hate this

--- Part2/MapOfItems.h
+++ Part2/MapOfItems.h
@@ -53,30 +53,36 @@ public:
         Item * currentItem = visitNext;
         Item * temp = nullptr;   
         //cout << temp << "\n";
-        int time = 0;
-        while (currentTime < timeLimit || visited.size() != items.size()){                      
+       
+        while (currentTime < timeLimit || visited.size() != items.size()){        
+            double fastestTime = __DBL_MAX__;            
             for (int i = 0; i < items.size(); ++i){
                 temp = &items[i];
                 if (seenItem(visited, temp) == false){
-                     //cout << temp <<"\n";
-                    time = temp->getTime()+900;
-                    if (visit(currentItem, temp, walkSpd) <= time){
-                        //visited.push_back(visitNext);
+                    //cout << temp <<" temp[\n";
+                    double vTime = visit(currentItem, temp, walkSpd);
+                    if (vTime > temp->getTime() && vTime <= (temp->getTime() + 900) && vTime < fastestTime){
+                        fastestTime = vTime;      
+                        //cout << fastestTime << " fast\n";                 
                         visitNext = temp;
-                        //cout <<" why\n";
+                        //visited.push_back(visitNext);
+                        //cout << visitNext << "/" << fastestTime <<" why\n";
                     }
                 }
 
             }
             visited.push_back(visitNext);
+            //cout << visited.back() << " back\n";
+            currentTime = visit(currentItem, visitNext, walkSpd);
             currentItem = visitNext;
             //cout << visited[visited.size()-1] << " last\n";
-            currentTime = visit(temp, visitNext, walkSpd);
+            cout << currentTime << " limimdime\n";           
 
-            if (visited.size() == items.size()){
+            if (visited.size() == items.size() || currentTime > 3600.0){
                 for (int i = 0; i< visited.size(); ++i){
                     cout << visited[i]->getTime() << " " << i << " list\n"; 
                 }
+                
                 break;
             }
 
@@ -88,14 +94,21 @@ public:
 
     bool seenItem(const vector<Item*> visited, const Item * itemIn) const{
         //cout << "chek\n";
+        static int count = 0;
+        static int cnt = 0;
         for (int i = 0; i< visited.size(); ++i){
             if(visited[i] == itemIn){
                 // cout << itemIn << " in\n";
                 // cout << visited[i] << " visited\n";
-
+                // count ++;
+                // cout << count << " count\n";
                 return true;
+                break;
             }
+            
         }
+        // cnt++;
+        // cout << cnt <<" asdf\n";
         return false;
     }
 
@@ -106,9 +119,6 @@ public:
         // cout << dist << " dist\n";
         // cout << walkTime << " walktime\n";
         // cout << walkTime + currentTime << " ret\n";
-
-        
-
         return walkTime + currentTime;
     }
     
