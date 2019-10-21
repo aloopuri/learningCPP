8112819af3036e31a94697948bfdcda0c9de5984
Hassan <k1889317@nmscde004164.nms.kcl.ac.uk>
2019-10-21 15:46:15 +0100

    prelim treenode done

--- Part4/treenode.h
+++ Part4/treenode.h
@@ -15,6 +15,49 @@ using std::pair;
 
 // TODO your code for the TreeNode class goes here:
 
+template <typename T>
+class TreeNode {
+
+public:
+
+    T data;
+    unique_ptr<TreeNode> leftChild;
+    unique_ptr<TreeNode> rightChild;
+    TreeNode * parent;
+
+    TreeNode(T dataIn) : data(dataIn) {
+        parent = nullptr;
+    }
+
+    void setLeftChild(TreeNode* child) {
+        leftChild.reset(child);
+        child->parent = this;
+    }
+
+    void setRightChild(TreeNode* child) {
+        rightChild.reset(child);
+        child->parent = this;
+    }
+
+    ostream & write(ostream & out) const {
+        if (leftChild != nullptr){
+            out << leftChild.get()->write(out);
+        }
+
+        out << " " << data << " ";
+
+        if (rightChild != nullptr){
+            out << rightChild.get()->write(out);
+        }
+
+        return out;
+    }
+};
+
+ostream & operator<<(ostream & o1, ostream & o2) {
+     return o2;
+}
+
 // do not edit below this line
 
 #endif
