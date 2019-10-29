c1345c675325328b5c8d437e0b21c783fdeb4af9
Hassan, Mohammad (k1889317) <mohammad.hassan@kcl.ac.uk>
2019-10-29 22:38:51 +0000

    write, insert and find done

--- Part4/tree.h
+++ Part4/tree.h
@@ -5,7 +5,85 @@
 
 // TODO your code goes here:
 
+template<typename T>
+class BinarySearchTree{
 
+private:
+    unique_ptr<TreeNode<T>> root = nullptr;
+
+public:
+    BinarySearchTree(){}
+
+    BinarySearchTree(const BinarySearchTree & other){
+        root = other.root;
+        
+    }
+
+    void write(ostream & out) const{
+        (root.get())->write(out);
+    }
+
+    TreeNode<T>* insert(T dataIn){
+        if (root == nullptr){
+            TreeNode<T>* newNode = new TreeNode<T>(dataIn);
+            root.reset(newNode);   
+            return root.get();         
+        }
+
+        TreeNode<T>* curNode = root.get();
+        TreeNode<T>* prevNode = nullptr;
+
+        while (curNode != nullptr){ // will end on an empty leaf
+            prevNode = curNode;     // this remembers the last node checked
+            if (dataIn < curNode->data){
+                curNode = curNode->leftChild.get();
+            }
+            else if (curNode->data < dataIn){
+                curNode = curNode->rightChild.get();
+            }
+            else {  // if the number isnt less/greater than, it must be the same
+                return curNode;
+            }
+            
+        }
+
+        if (dataIn < prevNode->data){
+            TreeNode<T>* newNode = new TreeNode<T>(dataIn);
+            prevNode->setLeftChild(newNode);
+            newNode->parent = prevNode;
+            return prevNode->leftChild.get();
+        }
+        else if (prevNode->data < dataIn){
+            TreeNode<T>* newNode = new TreeNode<T>(dataIn);
+            prevNode->setRightChild(newNode);
+            newNode->parent = prevNode;
+            return prevNode->rightChild.get();
+        }       
+        
+        return nullptr;
+    }
+
+    TreeNode<T>* find(T dataIn){
+        TreeNode<T>* curNode = root.get();
+        TreeNode<T>* prevNode = nullptr;
+
+        while(curNode != nullptr){ // will end on an empty leaf
+            prevNode = curNode;     // this remembers the last node checked
+            if (dataIn < curNode->data){
+                curNode = curNode->leftChild.get();
+            }
+            else if (curNode->data < dataIn){
+                curNode = curNode->rightChild.get();
+            }
+            else {
+                return curNode;
+            }           
+
+        }
+        return nullptr;
+    }
+    
+};
 
 
 // do not edit below this line
