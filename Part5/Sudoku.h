cb056d1261432ad83460a4591f61547384798a5b
Hassan <k1889317@nmscde005860.client.kcl.ac.uk>
2019-11-04 21:00:48 +0000

    what the actual frickedy frick

--- Part5/Sudoku.h
+++ Part5/Sudoku.h
@@ -16,37 +16,74 @@ using std::cout;
 
 // TODO: Your Sudoku class goes here:
 
-class Sudoku{  
+class Sudoku : public Searchable {  
 
 public:
     vector<vector<set<int>>> board;
     vector<vector<bool>> checked;
     int size;
 
-    Sudoku(int sizeIn) : size(sizeIn){  
+    Sudoku(int sizeIn) : Searchable(), size(sizeIn){  
+        makeBoard();
+        // set<int> setty;
+        // for(int i = 0; i < sizeIn; ++i){
+        //     setty.insert(i+1);
+        // }
+
+        // for (int i = 0; i< sizeIn; ++i){
+        //     vector<set<int>> vec;
+        //     for (int j = 0; j < sizeIn; ++j){                
+        //         vec.push_back(setty);
+        //     }
+        //     board.push_back(vec);
+        // }  
+
+        // for (int i = 0; i< sizeIn; ++i){
+        //     vector<bool> vec;
+        //     for (int j = 0; j < sizeIn; ++j){                
+        //         vec.push_back(false);
+        //     }
+        //     checked.push_back(vec);
+        // }  
+    }
+
+    Sudoku(const Sudoku & other){
+        size = other.size;
+        makeBoard();
+        //cout << "so\n";
+
+        for (int i = 0; i< size; ++i){
+            for (int j = 0; j < size; ++j){ 
+                board[i][j] = other.board[i][j]; 
+                checked[i][j] = other.checked[i][j];              
+            }
+        }                 
+    }
+
+    void makeBoard(){
         set<int> setty;
-        for(int i = 0; i < sizeIn; ++i){
+        for(int i = 0; i < size; ++i){
             setty.insert(i+1);
         }
 
-        for (int i = 0; i< sizeIn; ++i){
+        for (int i = 0; i< size; ++i){
             vector<set<int>> vec;
-            for (int j = 0; j < sizeIn; ++j){                
+            for (int j = 0; j < size; ++j){                
                 vec.push_back(setty);
             }
             board.push_back(vec);
         }  
 
-        for (int i = 0; i< sizeIn; ++i){
+        for (int i = 0; i< size; ++i){
             vector<bool> vec;
-            for (int j = 0; j < sizeIn; ++j){                
+            for (int j = 0; j < size; ++j){                
                 vec.push_back(false);
             }
             checked.push_back(vec);
         }  
     }
 
-    int getSquare(int row, int col){
+    int getSquare(int row, int col) const{
         if (board[row][col].size() == 1){
             return *board[row][col].begin();
         }
@@ -97,14 +134,74 @@ public:
                 if (getSquare(i,j) != -1 && checked[i][j] == false){
                     checked[i][j] = true;
                     setSquare(i,j,num);
-                    if(board[row][i].size() == 0){
+                    if(board[i][j].size() == 0){
                         return false;
                     }
                 }
             }
         }
+
+        for (int i = 0; i < size; ++i){
+            for (int j = 0; j < size; ++j){
+                if(board[i][j].size() == 0){
+                    return false;
+                }
+            }
+        }
         
         return true;
+    }
+
+    virtual bool isSolution() const override{
+        for (int i = 0; i < size; ++i){
+            for (int j = 0; j < size; ++j){
+                if (board[i][j].size() != 1){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    virtual void write(ostream & o) const override{
+        int count = 0;
+        for (int i = 0; i < size; ++i){
+            for (int j = 0; j < size; ++j){
+                for(auto k : board[i][j]){ 
+                    count++;
+                    cout << getSquare(i,j);   
+                    break;
+                }
+                if(j == size -1){
+                    cout << "\n"; 
+                }                    
+            }
+        }
+    }
+
+    virtual vector<unique_ptr<Searchable>> successors() const override{
+        vector<unique_ptr<Searchable>> succ;
+
+        for (int i = 0; i < size; ++i){
+            for (int j = 0; j < size; ++j){
+                if (board[i][j].size() > 1){
+                    //cout << "here?\n";
+                    set<int> leftSquare = board[i][0];
+                    Sudoku* newboard = new Sudoku(*this);
+                    if(newboard->setSquare(i,j, *leftSquare.begin())){  
+                        //setSquare(i,j, *leftSquare.begin());                      
+                        //unique_ptr(newboard);
+                        //cout << "isjsf\n";
+                        //newboard->write(cout);
+                        succ.push_back(unique_ptr<Searchable>(newboard));
+                    }    
+                    else {
+                        delete newboard;
+                    }                   
+                }                                
+            }
+        }
+        return succ;
     }
 
 
