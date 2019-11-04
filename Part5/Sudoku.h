a5bb601d949cef04e204bf979a90ea1ec3be4e7f
Hassan <k1889317@nmscde005860.client.kcl.ac.uk>
2019-11-04 17:45:05 +0000

    basic done

--- Part5/Sudoku.h
+++ Part5/Sudoku.h
@@ -20,12 +20,13 @@ class Sudoku{
 
 public:
     vector<vector<set<int>>> board;
+    vector<vector<bool>> checked;
     int size;
 
     Sudoku(int sizeIn) : size(sizeIn){  
         set<int> setty;
-        for(int k = 0; k < sizeIn; ++k){
-            setty.insert(k+1);
+        for(int i = 0; i < sizeIn; ++i){
+            setty.insert(i+1);
         }
 
         for (int i = 0; i< sizeIn; ++i){
@@ -34,32 +35,39 @@ public:
                 vec.push_back(setty);
             }
             board.push_back(vec);
-        }       
+        }  
+
+        for (int i = 0; i< sizeIn; ++i){
+            vector<bool> vec;
+            for (int j = 0; j < sizeIn; ++j){                
+                vec.push_back(false);
+            }
+            checked.push_back(vec);
+        }  
     }
 
     int getSquare(int row, int col){
-        if (board[row][col].size() > 1){
-            return -1;
+        if (board[row][col].size() == 1){
+            return *board[row][col].begin();
         }
         else {
-            return *board[row][col].begin();
+            return -1;
         }
     }
 
-    bool setSquare(int row, int col, int value){
-        
-        
+    bool setSquare(int row, int col, int value){       
         for(int i = 0; i < size; ++i){
             board[row][i].erase(value);     // RETURN FALSE IF EMPTY/ SIZE = O
         }
         for(int i = 0; i < size; ++i){
             board[i][col].erase(value);   
         }
+
         int boxSize = sqrt(size);
         int countX = 0;
         int x = row;
-        while (x >= 0){
-            x = x - boxSize;
+        while (x >= 0){     // checks how many subtractions are needed to make x<0
+            x = x - boxSize;    
             countX++;
         }
         countX--;
@@ -71,32 +79,32 @@ public:
             countY++;
         }
         countY--;
-        countX = countX * boxSize;
+        countX = countX * boxSize;  // multiplies by box size to give the top left corner of each sub-box
         countY = countY * boxSize;
 
-        for (int i = countX; i < countX + (boxSize-1); ++i){
-            for (int j = countY; j < countY + (boxSize-1); ++j){
+        for (int i = countX; i < countX + (boxSize); ++i){
+            for (int j = countY; j < countY + (boxSize); ++j){
                 board[i][j].erase(value);
             }
         }
-        cout << "BEFORE:\n";
-        cout << row <<", " << col << " pos:\n";
-        for(auto i : board[row][col]){            
-            cout << i << ", ";
-        }
-        cout << "\n";
+        
         board[row][col].clear();
-        board[row][col].insert(value);   
-        cout << "AFTER:\n";
-        cout << row <<", " << col << " pos:\n";
-        for(auto i : board[row][col]){            
-            cout << i << ", ";
-        }     
-        cout << "\n";
+        board[row][col].insert(value); 
 
+        for (int i = 0; i < size; ++i){
+            for (int j = 0; j < size; ++j){
+                int num = getSquare(i,j);
+                if (getSquare(i,j) != -1 && checked[i][j] == false){
+                    checked[i][j] = true;
+                    setSquare(i,j,num);
+                    if(board[row][i].size() == 0){
+                        return false;
+                    }
+                }
+            }
+        }
+        
         return true;
-
-
     }
 
 
